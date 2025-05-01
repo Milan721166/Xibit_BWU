afbb6a56667bff1299aab902df8cb7c5d4e454e6
Arman Mondal <admin@armanm.me>
2025-05-01 14:47:32 +0530

    modified:   frontend/src/App.tsx 	modified:   frontend/src/pages/LandingPage/MindMateLandingPage.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -93,7 +93,9 @@ export default function App() {
             {/* Dashboard Layout */}
 
             {/* Auth Layout */}
-            <Route path="/" element={<SignIn />} />
+            <Route path="/" element={<MindMateLandingPage />} />
+
+            <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
 
             {/* Fallback Route */}
@@ -101,46 +103,7 @@ export default function App() {
           </Routes>
         )}
 
-        <Routes>
-          {/* Landing Page Route (outside AppLayout) */}
-          <Route path="/landing" element={<MindMateLandingPage />} />
-          
-          {/* Dashboard Layout */}
-          <Route element={<AppLayout />}>
-            <Route index path="/" element={<Home />} />
-
-            {/* Others Page */}
-            <Route path="/profile" element={<UserProfiles />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/blank" element={<Blank />} />
-            <Route path="/wellness" element={<Wellness />} />
-
-            {/* Forms */}
-            <Route path="/form-elements" element={<FormElements />} />
-
-            {/* Tables */}
-            <Route path="/basic-tables" element={<BasicTables />} />
-
-            {/* Ui Elements */}
-            <Route path="/alerts" element={<Alerts />} />
-            <Route path="/avatars" element={<Avatars />} />
-            <Route path="/badge" element={<Badges />} />
-            <Route path="/buttons" element={<Buttons />} />
-            <Route path="/images" element={<Images />} />
-            <Route path="/videos" element={<Videos />} />
-
-            {/* Charts */}
-            <Route path="/line-chart" element={<LineChart />} />
-            <Route path="/bar-chart" element={<BarChart />} />
-          </Route>
-
-          {/* Auth Layout */}
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-
-          {/* Fallback Route */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      
       </Router>
     </>
   );
