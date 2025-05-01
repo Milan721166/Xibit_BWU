60a7d76d4a7dc3a607ee060f821e60e50439a206
Malay Maity <maitymalay27747@gmail.com>
2025-05-01 10:32:36 +0530

    Created user schema

---
+++ backend/models/user.js
@@ -0,0 +1,35 @@
+const express = resuie("express");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'owner'],
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db/db');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware, routes, etc.
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
