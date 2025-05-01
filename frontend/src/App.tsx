a2f11af229d64e1b6244dbab99fce9450ae60bac
rimanshupatel <rimanshupatel1@gmail.com>
2025-05-01 15:35:36 +0530

    modified:   frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -90,7 +90,6 @@ export default function App() {
         ) : (
           <Routes>
             {/* Dashboard Layout */}
-            <Route path="/wellness" element={<Wellness />} />
             {/* Auth Layout */}
             <Route path="/" element={<MindMateLandingPage />} />
 
