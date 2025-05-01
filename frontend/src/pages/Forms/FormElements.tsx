3ab7ecb71ac8e7432f063e63c963ecf8fd548f42
Arman Mondal <admin@armanm.me>
2025-05-01 15:08:30 +0530

    modified:   backend/db/generateAttandanceData.js 	modified:   frontend/src/pages/Forms/FormElements.tsx

--- frontend/src/pages/Forms/FormElements.tsx
+++ frontend/src/pages/Forms/FormElements.tsx
@@ -10,31 +10,63 @@ import SelectInputs from "../../components/form/form-elements/SelectInputs";
 import TextAreaInput from "../../components/form/form-elements/TextAreaInput";
 import InputStates from "../../components/form/form-elements/InputStates";
 import PageMeta from "../../components/common/PageMeta";
+import Select from "../../components/form/Select";
+import { useEffect, useState } from "react";
+import { api } from "../../config/api";
+import axios from "axios";
 
 export default function FormElements() {
+  const [students, setStudents] = useState([]);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+  useEffect(()=>{
+    const fetchStudents = async () => {
+      try {
+        const response = await axios.get(api+'userRoutes/users');
+        if(response.status === 200) {
+          setStudents(response.data.users);
+        }
+
+      } catch (error) {
+        console.error('Error fetching students:', error);
+      }
+    };
+    fetchStudents();
+
+
+  },[])
   return (
     <div>
       <PageMeta
         title="React.js Form Elements Dashboard | TailAdmin - React.js Admin Dashboard Template"
         description="This is React.js Form Elements  Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="From Elements" />
-      <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
-        <div className="space-y-6">
-          <DefaultInputs />
-          <SelectInputs />
-          <TextAreaInput />
-          <InputStates />
-        </div>
-        <div className="space-y-6">
-          <InputGroup />
-          <FileInputExample />
-          <CheckboxComponents />
-          <RadioButtons />
-          <ToggleSwitch />
-          <DropzoneComponent />
-        </div>
-      </div>
-    </div>
+      <PageBreadcrumb pageTitle="Attandence" />
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="grid grid-cols-1 gap-4 mt-4 md:grid-cols-2 lg:grid-cols-3">
+            <div className="col-span-1 md:col-span-2 lg:col-span-3">
+              <h1 className="text-2xl font-semibold leading-tight">Attendance Form</h1>
+              <p className="mt-2 text-sm text-gray-600">Please fill out the form below to mark attendance.</p>
+            </div>
+<div className="col-span-1 md:col-span-2 py-2 lg:col-span-3">
+<h1 className="text-2xl font-semibold leading-tight">Select Student</h1>
+            <Select
+             
+              options={students.map((student) => ({
+                value: student?._id,
+                label: `${student?.name}`,
+              }))}
+              onChange={(selectedOption:any) => {
+                setSelectedStudent(selectedOption);
+                console.log(selectedOption);
+              }}
+            />
+            </div>
+</div>
+</div>
+</div>
+</div>
+
+     
   );
 }
